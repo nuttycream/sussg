c1a745394d6d847ef83fb3ddf92c9c5153f7a71d
nuttyCream <issanutty@gmail.com>
2025-08-24 21:36:54 -0700

    refactor: mv serve

---
+++ src/serve.rs
@@ -0,0 +1,49 @@
+use axum::Router;
+use tokio::signal;
+use tower_http::services::ServeDir;
+use tower_livereload::LiveReloadLayer;
+
+#[tokio::main]
+pub async fn serve() {
+    let static_files = ServeDir::new("./public");
+
+    // todo: cannot use "/" to server static files
+    // need workaround
+    let app = Router::new()
+        .nest_service("/index", static_files)
+        .layer(LiveReloadLayer::new());
+
+    let listener = tokio::net::TcpListener::bind("127.0.0.1:3030")
+        .await
+        .unwrap();
+
+    println!("listening on {}", listener.local_addr().unwrap());
+    axum::serve(listener, app)
+        .with_graceful_shutdown(shutdown_signal())
+        .await
+        .unwrap();
+}
+
+async fn shutdown_signal() {
+    let ctrl_c = async {
+        signal::ctrl_c()
+            .await
+            .expect("failed to install Ctrl+C handler");
+    };
+
+    #[cfg(unix)]
+    let terminate = async {
+        signal::unix::signal(signal::unix::SignalKind::terminate())
+            .expect("failed to install signal handler")
+            .recv()
+            .await;
+    };
+
+    #[cfg(not(unix))]
+    let terminate = std::future::pending::<()>();
+
+    tokio::select! {
+        _ = ctrl_c => {},
+        _ = terminate => {},
+    }
+}
