a6c8d778238b1796a862f058d2b2237bd5d3aa3f
nuttyCream <issanutty@gmail.com>
2025-08-25 16:15:53 -0700

    refactor: factordis

---
+++ src/build.rs
@@ -0,0 +1,64 @@
+use std::{
+    fs::{self, File},
+    io::Write,
+    path::Path,
+};
+
+use crate::convert;
+
+pub fn build() {
+    match fs::create_dir("public") {
+        Ok(_) => println!("./public created successfully"),
+        Err(e) => println!("failed to create ./public: {e}"),
+    };
+
+    let contents = fs::read_dir("./content").unwrap();
+    let styles_dir = fs::read_dir("./styles").unwrap();
+
+    let mut styles = Vec::new();
+    for style in styles_dir {
+        let entry = style.unwrap();
+        let path = entry.path();
+        if let Some(extension) = path.extension()
+            && extension == "css"
+        {
+            let file_name = path.file_name().unwrap();
+            let dest_path = Path::new("./public").join(file_name);
+            fs::copy(&path, &dest_path).unwrap();
+            println!(
+                "copying {} to ./public/{}",
+                path.display(),
+                file_name.to_string_lossy()
+            );
+
+            styles.push(file_name.to_string_lossy().to_string());
+        }
+    }
+
+    for content in contents {
+        let entry = content.unwrap();
+        let path = entry.path();
+        if let Some(extension) = path.extension()
+            && extension == "md"
+        {
+            if path.file_stem().unwrap() == "index" {
+                let md_string = fs::read_to_string(&path).unwrap();
+                let html = convert(md_string, styles.clone());
+                let root = File::create("./public/index.html");
+                root.and_then(|mut file| file.write_all(html.as_bytes()))
+                    .map(|_| println!("index created successfully"))
+                    .unwrap_or_else(|e| println!("failed to create index.html: {e}"));
+            } else if path.is_file() {
+                let md_string = fs::read_to_string(&path).unwrap();
+                let html = convert(md_string, styles.clone());
+                let root = File::create(format!(
+                    "./public/{}.html",
+                    path.file_name().unwrap().to_str().unwrap()
+                ));
+                root.and_then(|mut file| file.write_all(html.as_bytes()))
+                    .map(|_| println!("created successfully"))
+                    .unwrap_or_else(|e| println!("failed to create index.html: {e}"));
+            }
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,8 @@
+pub mod build;
 pub mod convert;
+pub mod init;
 pub mod serve;
 pub mod toml_stuff;
-
-use std::{
-    fs::{self, File},
-    io::Write,
-    path::Path,
-};
 
 use clap::{Parser, Subcommand};
 
@@ -30,89 +26,8 @@ fn main() {
     let args = Args::parse();
 
     match &args.command {
-        Commands::Init => init(),
-        Commands::Build => build(),
+        Commands::Init => init::init(),
+        Commands::Build => build::build(),
         Commands::Serve => serve::serve(),
-    }
-}
-
-fn init() {
-    match fs::create_dir("content") {
-        Ok(_) => println!("./content created successfully"),
-        Err(e) => println!("failed to create ./content: {e}"),
-    };
-
-    match fs::create_dir("styles") {
-        Ok(_) => println!("./styles created successfully"),
-        Err(e) => println!("failed to create ./styles: {e}"),
-    };
-
-    File::create("Config.toml")
-        .and_then(|mut file| file.write_all("test4testes".as_bytes()))
-        .map(|_| println!("config created successfully"))
-        .unwrap_or_else(|e| println!("failed to create config: {e}"));
-
-    println!(
-        "
-finished initializing ssssg start creating your markdown
-content in the content folder, css goes into the styles folder,
-then run 'ssssg serve' to preview locally.
-    "
-    );
-}
-
-fn build() {
-    match fs::create_dir("public") {
-        Ok(_) => println!("./public created successfully"),
-        Err(e) => println!("failed to create ./public: {e}"),
-    };
-
-    let contents = fs::read_dir("./content").unwrap();
-    let styles_dir = fs::read_dir("./styles").unwrap();
-    let mut styles = Vec::new();
-    for style in styles_dir {
-        let entry = style.unwrap();
-        let path = entry.path();
-        if let Some(extension) = path.extension()
-            && extension == "css"
-        {
-            let file_name = path.file_name().unwrap();
-            let dest_path = Path::new("./public").join(file_name);
-            fs::copy(&path, &dest_path).unwrap();
-            println!(
-                "copying {} to ./public/{}",
-                path.display(),
-                file_name.to_string_lossy()
-            );
-
-            styles.push(file_name.to_string_lossy().to_string());
-        }
-    }
-
-    for content in contents {
-        let entry = content.unwrap();
-        let path = entry.path();
-        if let Some(extension) = path.extension()
-            && extension == "md"
-        {
-            if path.file_name().unwrap() == "index.md" {
-                let md_string = fs::read_to_string(&path).unwrap();
-                let html = convert(md_string, styles.clone());
-                let root = File::create("./public/index.html");
-                root.and_then(|mut file| file.write_all(html.as_bytes()))
-                    .map(|_| println!("index created successfully"))
-                    .unwrap_or_else(|e| println!("failed to create index.html: {e}"));
-            } else if path.is_file() {
-                let md_string = fs::read_to_string(&path).unwrap();
-                let html = convert(md_string, styles.clone());
-                let root = File::create(format!(
-                    "./public/{}.html",
-                    path.file_name().unwrap().to_str().unwrap()
-                ));
-                root.and_then(|mut file| file.write_all(html.as_bytes()))
-                    .map(|_| println!("created successfully"))
-                    .unwrap_or_else(|e| println!("failed to create index.html: {e}"));
-            }
-        }
     }
 }
