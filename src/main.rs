f895eb8b4056be89eef81353dc669e958b9efbfd
nuttyCream <issanutty@gmail.com>
2025-08-24 21:37:11 -0700

    feat: impl styles to html link

--- src/main.rs
+++ src/main.rs
@@ -1,15 +1,13 @@
 pub mod md_to_html;
+pub mod serve;
 
 use std::{
     fs::{self, File},
     io::Write,
+    path::{self, Path},
 };
 
-use axum::Router;
 use clap::{Parser, Subcommand};
-use tokio::signal::{self};
-use tower_http::services::ServeDir;
-use tower_livereload::LiveReloadLayer;
 
 use crate::md_to_html::convert;
 
@@ -33,7 +31,7 @@ fn main() {
     match &args.command {
         Commands::Init => init(),
         Commands::Build => build(),
-        Commands::Serve => serve(),
+        Commands::Serve => serve::serve(),
     }
 }
 
@@ -69,62 +67,37 @@ fn build() {
     };
 
     let contents = fs::read_dir("./content").unwrap();
+    let styles_dir = fs::read_dir("./styles").unwrap();
+    let mut styles = Vec::new();
+    for style in styles_dir {
+        let entry = style.unwrap();
+        let path = entry.path();
+        if let Some(extension) = path.extension()
+            && extension == "css"
+        {
+            let file_name = path.file_name().unwrap();
+            let dest_path = Path::new("./public").join(file_name);
+            fs::copy(&path, &dest_path).unwrap();
+            println!(
+                "copying {} to ./public/{}",
+                path.display(),
+                file_name.to_string_lossy()
+            );
+
+            styles.push(file_name.to_string_lossy().to_string());
+        }
+    }
 
     for content in contents {
         let entry = content.unwrap();
         let path = entry.path();
         if path.file_name().unwrap() == "index.md" {
             let md_string = fs::read_to_string(&path).unwrap();
-            let html = convert(md_string);
+            let html = convert(md_string, styles.clone());
             let root = File::create("./public/index.html");
             root.and_then(|mut file| file.write_all(html.as_bytes()))
                 .map(|_| println!("index created successfully"))
                 .unwrap_or_else(|e| println!("failed to create index.html: {e}"));
         }
-    }
-}
-
-#[tokio::main]
-async fn serve() {
-    let static_files = ServeDir::new("./public");
-
-    // todo: cannot use "/" to server static files
-    // need workaround
-    let app = Router::new()
-        .nest_service("/index", static_files)
-        .layer(LiveReloadLayer::new());
-
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3030")
-        .await
-        .unwrap();
-
-    println!("listening on {}", listener.local_addr().unwrap());
-    axum::serve(listener, app)
-        .with_graceful_shutdown(shutdown_signal())
-        .await
-        .unwrap();
-}
-
-async fn shutdown_signal() {
-    let ctrl_c = async {
-        signal::ctrl_c()
-            .await
-            .expect("failed to install Ctrl+C handler");
-    };
-
-    #[cfg(unix)]
-    let terminate = async {
-        signal::unix::signal(signal::unix::SignalKind::terminate())
-            .expect("failed to install signal handler")
-            .recv()
-            .await;
-    };
-
-    #[cfg(not(unix))]
-    let terminate = std::future::pending::<()>();
-
-    tokio::select! {
-        _ = ctrl_c => {},
-        _ = terminate => {},
     }
 }
--- src/md_to_html.rs
+++ src/md_to_html.rs
@@ -1,12 +1,18 @@
 use pulldown_cmark_frontmatter::FrontmatterExtractor;
 
-pub fn convert(md_string: String) -> String {
+pub fn convert(md_string: String, styles: Vec<String>) -> String {
     extract_frontmatter(md_string.clone());
 
     let parser = pulldown_cmark::Parser::new(&md_string);
-
     let mut html_output = String::new();
     pulldown_cmark::html::push_html(&mut html_output, parser);
+
+    let mut link = String::new();
+    for style in styles {
+        link.push_str(&format!("<link rel=\"stylesheet\" href=\"{}\">\n", style));
+    }
+
+    html_output = link + &html_output;
 
     html_output
 }
