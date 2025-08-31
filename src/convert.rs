f6d873a23be4c9d2807fab92eb2921e0302df5ad
nuttyCream <issanutty@gmail.com>
2025-08-31 04:39:09 -0700

    refactor out style, we wont handle it there

--- src/convert.rs
+++ src/convert.rs
@@ -1,6 +1,6 @@
 use pulldown_cmark_frontmatter::FrontmatterExtractor;
 
-pub fn convert(md_string: String, styles: Vec<String>) -> (String, String) {
+pub fn convert(md_string: &String) -> (String, String) {
     let mut extractor = FrontmatterExtractor::new(pulldown_cmark::Parser::new(&md_string));
     let mut html_output = String::new();
     pulldown_cmark::html::push_html(&mut html_output, &mut extractor);
@@ -16,11 +16,5 @@ pub fn convert(md_string: String, styles: Vec<String>) -> (String, String) {
 
     // code_block.source.into_string()
 
-    let mut link = String::new();
-    for style in styles {
-        link.push_str(&format!("<link rel=\"stylesheet\" href=\"{}\">\n", style));
-    }
-
-    html_output = link + &html_output;
     (code_block, html_output)
 }
