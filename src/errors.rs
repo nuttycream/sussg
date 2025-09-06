06b575f1d3d910165ce28009b6b6590d55d74038
nuttyCream <issanutty@gmail.com>
2025-09-06 16:39:44 -0700

    feat: add errors

---
+++ src/errors.rs
@@ -0,0 +1,35 @@
+use core::fmt;
+use std::error::Error;
+
+#[derive(Debug)]
+pub enum ErrDis {
+    BadDirectory,
+    BadPage(String),
+    BadMarkdown(String),
+    BadMarkdownString(String),
+    BadFrontmatter(String, String),
+}
+
+impl Error for ErrDis {}
+
+impl fmt::Display for ErrDis {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        match self {
+            ErrDis::BadDirectory => {
+                write!(f, "Bad Directory")
+            }
+            ErrDis::BadPage(e) => {
+                write!(f, "Failed to read page: {e}")
+            }
+            ErrDis::BadMarkdown(e) => {
+                write!(f, "Failed to read markdown: {e}")
+            }
+            ErrDis::BadMarkdownString(e) => {
+                write!(f, "Failed to convert string to markdown: {e}")
+            }
+            ErrDis::BadFrontmatter(fm, e) => {
+                write!(f, "Failed to parse frontmatter: {fm}\nBecause:{e}")
+            }
+        }
+    }
+}
