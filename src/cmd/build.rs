0a99ac03261eaaa935dc1a0314d8d9f7f4ff927f
nuttyCream <issanutty@gmail.com>
2025-09-06 00:44:49 -0700

    init posts - remove that block of code btw

--- src/cmd/build.rs
+++ src/cmd/build.rs
@@ -31,6 +31,15 @@ struct RenderedContent {
     content: String,
 }
 
+// from /content/posts
+// this should all be gathered
+// from the frontmatter
+struct Post {
+    title: String,
+    description: Option<String>,
+    date: Option<String>,
+}
+
 // Can be anything, a post,
 // a page, a SR-71 BlackBird
 #[derive(Debug)]
@@ -40,13 +49,6 @@ struct TheThing {
     styles: Vec<Style>,
     mustache: Mustache,
     content: String,
-}
-
-// gonna use this as a global thing
-// to manage posts ya?
-struct BiggerThing {
-    posts: Vec<TheThing>,
-    pages: Vec<TheThing>,
 }
 
 pub fn build() {
@@ -63,11 +65,6 @@ pub fn build() {
         }
         Err(e) => println!("failed to somehow check if public exists: {e}"),
     }
-
-    let bigger_thing = BiggerThing {
-        posts: Vec::new(),
-        pages: Vec::new(),
-    };
 
     // maybe add some optimizations to images here? hmmmm?
     for static_file in WalkDir::new("./static").into_iter().filter_map(|e| e.ok()) {
@@ -149,9 +146,111 @@ pub fn build() {
 
     println!("avail_styles:{:?}", styles);
     println!("avail_templs:{:?}", mustaches);
+
+    let mut posts: Vec<Post> = Vec::new();
     for content in WalkDir::new("./content").into_iter().filter_map(|e| e.ok()) {
-        if content.path().starts_with("posts") {
-            if content.file_type().is_dir() {}
+        if content.path().starts_with("posts") && content.file_type().is_dir() {
+            let path = content.path();
+            let name = path.file_name().unwrap().to_str().unwrap().to_string();
+            let md_string = fs::read_to_string(path).unwrap();
+
+            println!("processing post:{}", path.display());
+
+            let (frontmatter_string, content) = convert(&md_string);
+
+            let frontmatter: Frontmatter = serde_yaml::from_str(&frontmatter_string)
+                .expect("could not convert frontmatter to struct");
+
+            //println!("{:?}", frontmatter);
+
+            let mut thing_styles = Vec::new();
+            if let Some(styles_strings) = frontmatter.styles {
+                for style in &styles {
+                    if styles_strings.contains(&style.name) {
+                        thing_styles.push(style.clone());
+                    }
+                }
+            }
+
+            // todo get this from mustache bro
+            let mut thing_mustache = mustaches
+                .iter()
+                .find(|m| m.name == "base")
+                .cloned()
+                .unwrap();
+
+            if let Some(mustache_name) = frontmatter.template {
+                println!("{:?}", mustache_name);
+                for avail_mustache in &mustaches {
+                    if avail_mustache.name == mustache_name {
+                        thing_mustache = avail_mustache.clone();
+                    }
+                }
+            }
+
+            println!("loaded_styles:{:?}", thing_styles);
+            println!("loaded_templ:{:?}", thing_mustache);
+
+            // HOLY FUCK LMAO
+            let relative_path = path.strip_prefix("./content").unwrap();
+            let count = relative_path.components().count();
+            let redirect_path = if count <= 1 {
+                "./"
+            } else {
+                &format!("{}/", "..".repeat(count - 1))
+            };
+
+            // thing is only used when building
+            // so we can pragmatically store what we need
+            // to build that file out
+            // methinks :shrug:
+            let mut thing = TheThing {
+                name,
+                path: path.to_str().unwrap().to_string(),
+                content,
+                styles: thing_styles,
+                mustache: thing_mustache,
+            };
+
+            posts.push(Post {
+                title: frontmatter.title.clone(),
+                description: frontmatter.description,
+                date: frontmatter.date,
+            });
+
+            (0..thing.styles.len()).for_each(|n| {
+                thing.styles.index_mut(n).path =
+                    format!("{}{}", redirect_path, thing.styles.index(n).path);
+            });
+
+            // now build out the html
+            let source = fs::read_to_string(thing.mustache.path).expect("mustache path is invalid");
+            let tpl = Template::new(source).unwrap();
+
+            let mut rendered = tpl.render(&RenderedContent {
+                title: frontmatter.title,
+                content: thing.content,
+            });
+
+            let mut link = String::new();
+            for style in thing.styles {
+                link.push_str(&format!(
+                    "<link rel=\"stylesheet\" href=\"{}\">\n",
+                    style.path
+                ));
+            }
+
+            rendered = link + &rendered;
+
+            //println!("{}\n", rendered);
+            let out = Path::new("./public")
+                .join(relative_path)
+                .with_extension("html");
+
+            fs::create_dir_all(out.parent().unwrap()).unwrap();
+
+            fs::write(&out, rendered).unwrap();
+            println!("created: {}", out.display());
         } else if content.path().extension() == Some(OsStr::new("md")) {
             let path = content.path();
             let name = path.file_name().unwrap().to_str().unwrap().to_string();
