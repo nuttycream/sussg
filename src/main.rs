52845f3677f30dfaa770df31b2ad743053bd0c2c
nuttyCream <issanutty@gmail.com>
2026-03-26 20:28:34 -0700

    refactor(cli): replace clap with manual argument parsing

--- src/main.rs
+++ src/main.rs
@@ -7,37 +7,58 @@ pub mod utils;
 
 use std::path::PathBuf;
 
-use clap::{Parser, Subcommand};
-
-#[derive(Parser, Debug)]
-#[command(version, about, long_about = None)]
-struct Args {
-    #[command(subcommand)]
-    command: Commands,
-}
-
-#[derive(Subcommand, Debug)]
-enum Commands {
-    Init,
-    Build {
-        #[arg(short, long, default_value = "./")]
-        path: PathBuf,
-    },
-    Serve {
-        #[arg(short, long, default_value = "./")]
-        path: PathBuf,
-
-        #[arg(long, default_value_t = 3030)]
-        port: u32,
-    },
+fn usage() {
+    println!("usage:");
+    println!("sussg init");
+    println!("sussg build [-p|--path <path>]");
+    println!("sussg serve [-p|--path <path>] [--port <port>]");
 }
 
 fn main() {
-    let args = Args::parse();
+    let args = std::env::args().collect::<Vec<String>>();
 
-    match &args.command {
-        Commands::Init => cmd::init::init(),
-        Commands::Build { path } => cmd::build::build(path, false).unwrap(),
-        Commands::Serve { path, port } => cmd::serve::serve(path, *port).unwrap(),
+    if args.len() < 2 {
+        usage();
+        return;
     }
+
+    match args[1].as_str() {
+        "init" => cmd::init::init(),
+        "build" => {
+            let path = path_arg(&args[2..]).unwrap_or_else(|| PathBuf::from("./"));
+
+            cmd::build::build(&path, false).unwrap();
+        }
+        "serve" => {
+            let path = path_arg(&args[2..]).unwrap_or_else(|| PathBuf::from("./"));
+
+            let port = port_arg(&args[2..]).unwrap_or(3030);
+
+            cmd::serve::serve(&path, port).unwrap();
+        }
+        other => {
+            println!("unknown cmd: {other}");
+            usage();
+        }
+    }
+}
+
+fn path_arg(args: &[String]) -> Option<PathBuf> {
+    args.windows(2).find_map(|pair| {
+        if pair[0] == "-p" || pair[0] == "--path" {
+            Some(PathBuf::from(&pair[1]))
+        } else {
+            None
+        }
+    })
+}
+
+fn port_arg(args: &[String]) -> Option<u32> {
+    args.windows(2).find_map(|pair| {
+        if pair[0] == "--port" {
+            pair[1].parse().ok()
+        } else {
+            None
+        }
+    })
 }
